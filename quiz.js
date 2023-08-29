07ad65c8a9161757bd6ee348a3067c36b51fd40e
panther865 <77604848+panther865@users.noreply.github.com>
2023-08-29 18:57:56 +0530

    timer done

---
+++ quiz.js
@@ -0,0 +1,15 @@
+var time = 15;
+let timer = document.getElementById("timer");
+function countdown() {
+    if (time == 0) {
+        alert("time up");
+    }
+    else {
+        if(time<5){
+            document.getElementById("timer").style.color="#cf4f4fb9";
+        }
+        time--;
+        document.getElementById("timer").innerHTML = time.toString();
+        setTimeout('countdown()', 1000);
+    }
+}
